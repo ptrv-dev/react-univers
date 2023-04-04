63b3b28c6344b970388d163ae177af65cf8f5490
ptrv-dev <petrov99119@gmail.com>
2023-04-04 16:36:36 +0300

    Responsive Section About

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
+
+import SectionHeader from '../SectionHeader';
 
 import style from './AboutSection.module.scss';
 
+import { aboutData } from '../../data/about';
+
 const AboutSection: React.FC = () => {
-  return <div>AboutSection</div>;
+  const [width] = useWindowSize();
+  return (
+    <div className={[style['about'], 'section'].join(' ')}>
+      <div className={[style['about__container'], 'container'].join(' ')}>
+        <SectionHeader
+          title="Get to know more about Univers"
+          subtitle="We are customer - focused digital bank that provides complete financial solutions to meet your entire financial needs"
+          variant={width > 768 ? 'row' : 'col'}
+          color="black"
+        />
+        <div className={style['about__body']}>
+          {aboutData.map(({ title, text }, idx) => (
+            <div key={idx} className={style['about-item']}>
+              <h3 className={style['about-item__title']}>{title}</h3>
+              <p className={style['about-item__text']}>{text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutSection;
---
+++ src/data/about.js
@@ -0,0 +1,14 @@
+export const aboutData = [
+  {
+    title: '250k',
+    text: 'Univers’s from all over the globe',
+  },
+  {
+    title: '500m',
+    text: 'Univers’s revenue in 2022 in the first quarter',
+  },
+  {
+    title: '280%',
+    text: 'Univers’s investment growth in 2023',
+  },
+];
