d4ebeba37078b05906be5d7e00e6e90f0a94cbb7
ptrv-dev <petrov99119@gmail.com>
2023-04-05 18:09:09 +0300

    Responsive Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Button from '../Button';
 
 import style from './Footer.module.scss';
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={style['footer']}>
+      <div className={style['footer__wrapper']}>
+        <div className={[style['footer__container'], 'container'].join(' ')}>
+          <h2 className={style['footer__title']}>
+            Connect your finance with Carty
+          </h2>
+          <Button variant="large">Join the universe</Button>
+          <p className={style['footer__copyright']}>
+            2023 © All right reserved (Univers™)
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
